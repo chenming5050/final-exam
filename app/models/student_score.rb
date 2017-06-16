ce48e28da2eae0fcb361d85e2023b5cba74208d0
chenming5050 <chenming5050@gmail.com>
2017-06-16 02:55:38 -0400

    updata display_score commit

--- app/models/student_score.rb
+++ app/models/student_score.rb
@@ -1,20 +1,20 @@
 class StudentScore < ApplicationRecord
   def display_score
      
-    if self.score >= 90 
-      rank = "S"
+    if self.score < 60 
+      rank = "D"
     end
-    if self.score >= 80 
-      rank = "A"
-    end
-    if self.score >= 70 
-      rank = "B"
-    end
-    if self.score >= 60 
+    if self.score < 70 
       rank = "C"
     end
-    if self.score < 60
-      rank = "D"
+    if self.score < 80 
+      rank = "B"
+    end
+    if self.score < 90 
+      rank = "A"
+    end
+    if self.score >= 90
+      rank = "S"
     end          
     return rank+" 級(#{score})"
   end
