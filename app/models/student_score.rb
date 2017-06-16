1a76c5744c4e5b51684f64c4a27d116f8ac2784e
chenming5050 <chenming5050@gmail.com>
2017-06-16 02:50:52 -0400

    display_score commit

--- app/models/student_score.rb
+++ app/models/student_score.rb
@@ -1,2 +1,21 @@
 class StudentScore < ApplicationRecord
+  def display_score
+     
+    if self.score >= 90 
+      rank = "S"
+    end
+    if self.score >= 80 
+      rank = "A"
+    end
+    if self.score >= 70 
+      rank = "B"
+    end
+    if self.score >= 60 
+      rank = "C"
+    end
+    if self.score < 60
+      rank = "D"
+    end          
+    return rank+" 級(#{score})"
+  end
 end
